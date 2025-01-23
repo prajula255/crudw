dce5cf0869ff4ccf3268e4fff071880fff3876cb
prajula255 <prajulasouparnika3@gmail.com>
2025-01-23 10:43:22 +0530

    redux toolkit installed

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store.js';
 
 createRoot(document.getElementById('root')).render(
-  
+
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
