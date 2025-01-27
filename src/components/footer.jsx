d09aa27bac258bdf27f2c710b3437337033bdfb1
prajula255 <prajulasouparnika3@gmail.com>
2025-01-27 11:18:43 +0530

    navbar and footer component added

---
+++ src/components/footer.jsx
@@ -0,0 +1,28 @@
+function FooterEx() {
+    return (
+        <>
+            <footer>
+                <div className='d-flex flex-wrap bg-primary-subtle text-dark justify-content-evenly p-2 gap-2 '>
+                    <div>
+                        <h6>ABOUT</h6>
+                        <p>Tech@OLX</p>
+                    </div>
+
+                    <div>
+                        <h6>OLX</h6>
+
+                        <p>Blog</p>
+                        <p>Help</p>
+                        <p>T&C</p>
+                    </div>
+
+                    <div>
+                        <h6>Contact</h6>
+                        <p>OLX trading Mumbai</p>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
+export default FooterEx;
--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './homePage.css'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import NavEx from '../../components/navbar';
+import FooterEx from '../../components/footer';
 function HomePage() {
   return (
     <>
+      <NavEx />
+
       <div className="home-container">
-        {/* <h1>hii</h1>  */}
-        <Card className="home-card">
+        <Card className="home-card" >
           <Card.Img variant="top" src="/public/Swift.jpg" alt="Card image" />
           <Card.Body>
             <Card.Title><h2>Maruti Suzuki Swift(2023)</h2></Card.Title>
@@ -35,6 +38,7 @@ function HomePage() {
           </Card.Body>
         </Card>
       </div>
+      <FooterEx/>
     </>
   );
 }
