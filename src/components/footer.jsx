44152f4026520dfe8364a921579152c738ca4195
prajula255 <prajulasouparnika3@gmail.com>
2025-01-27 12:25:41 +0530

    footer changes in styling done

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ function FooterEx() {
     return (
         <>
             <footer>
-                <div className='d-flex flex-wrap bg-primary-subtle text-dark justify-content-evenly p-2 gap-2 '>
+                <div className='d-flex flex-wrap bg-primary-subtle text-dark justify-content-evenly p-5 gap-2 '>
                     <div>
                         <h6>ABOUT</h6>
                         <p>Tech@OLX</p>
