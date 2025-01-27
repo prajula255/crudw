f9e44ed879534cb404c693a18e0fced11c0dd72d
prajula255 <prajulasouparnika3@gmail.com>
2025-01-27 10:18:49 +0530

    description added in card homepage

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -8,13 +8,29 @@ function HomePage() {
       <div className="home-container">
         {/* <h1>hii</h1>  */}
         <Card className="home-card">
-          <Card.Img variant="top" src="path/to/your/image.jpg" alt="Card image" />
+          <Card.Img variant="top" src="/public/Swift.jpg" alt="Card image" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title><h2>Maruti Suzuki Swift(2023)</h2></Card.Title>
+
+            <Card.Title>Description</Card.Title>
             <Card.Text>
-              This is a simple card component that can be used to display content.
+              *SWIFT ZXI*
+              *2021 MODEL*
+              *KM 60000*
+              *SERVICE DONE*
+              *SINGLE OWNER*
+              *4 NEW TYRES*
+              *NEW INSURANCE*
+              *INSURANCE UP TO*
+              *06 - 09 - 2025*
+              *COMPANY SERVICE*
+              *NO ACCIDENT*
+              *NO REPLACEMENT*
+              ADDITIONAL VEHICLE INFORMATION:
+              Accidental: No
+              Service History: Available
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Button variant="secondary">Chat with seller</Button>
 
           </Card.Body>
         </Card>
