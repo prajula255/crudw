58280bf132ab1cfc3a5331f510244a1b32809c4a
prajula255 <prajulasouparnika3@gmail.com>
2025-01-28 12:54:18 +0530

    font awesome icons added in productdetails

--- src/pages/productDetails/productDetails.jsx
+++ src/pages/productDetails/productDetails.jsx
@@ -44,9 +44,9 @@ function ProductDetails() {
             <div className='product-name'>
                 <div className='product-namecard'>
                     <h2>Maruti Suzuki Swift(2020)</h2>
-                    <h6>Petrol</h6>
-                    <h6>100132km</h6>
-                    <h6>Manual</h6>
+                    <h6><i class="fa-solid fa-gas-pump" />Petrol</h6>
+                    <h6><i class="fa-solid fa-gauge"></i>100132km</h6>
+                    <h6><i class="fa-solid fa-sitemap"></i>Manual</h6>
                 </div>
             </div>
 
@@ -54,9 +54,9 @@ function ProductDetails() {
             <div className='product-overview'>
                 <div className='product-overview-card'>
                     <h3>Overview</h3>
-                    <h6>1st owner</h6>
-                    <h6>Location</h6>
-                    <h6>Posting Date</h6>
+                    <h6><i class="fa-solid fa-user" />1st owner</h6>
+                    <h6><i class="fa-solid fa-location-dot" />Location</h6>
+                    <h6><i class="fa-solid fa-calendar-days" />Posting Date</h6>
                 </div>
             </div>
 
