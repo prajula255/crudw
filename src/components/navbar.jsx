14960c0778169c12fd5044b09cbd71f81d530ecb
prajula255 <prajulasouparnika3@gmail.com>
2025-02-07 14:40:07 +0530

    icons newly added in navigation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -18,23 +18,17 @@ function NavEx() {
                         navbarScroll
                     >
                         <Nav.Link href="#action1">Home</Nav.Link>
-                        <Form className="d-flex ">
+                        <Form className="d-flex  " style={{ width: "900px" }}>
                             <Form.Control
                                 type="search"
                                 placeholder="Search"
-                                className="me-2"
+                                className="me-2 "
                                 aria-label="Search"
                             />
                             <Button variant="outline-success">Search</Button>
+
                         </Form>
 
-                        {/* <Form className="d-flex">
-                            <Form.Control
-                                type="search"
-                                placeholder="Location"
-                                className="me-2"
-                                aria-label="Search"
-                            /> */}
                         <NavDropdown title="English" id="navbarScrollingDropdown">
 
                             <NavDropdown.Item href="#action3">English</NavDropdown.Item>
@@ -44,15 +38,36 @@ function NavEx() {
                                 Hindi                                </NavDropdown.Item>
 
                         </NavDropdown>
-                        <div className='d-flex align-items-center'>
+                        <div className='d-flex align-items-center gap-3'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6" height={24} width={24}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                             </svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6" height={24} width={24}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
+                            </svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6" height={24} width={24}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                            </svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6" height={24} width={24}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+
                         </div>
 
-                        <Nav.Link href="#action1">Login</Nav.Link>
+                        {/* <Button variant='outline-success' className='btn rounded-pill px-4  '>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
+                                Sell</Button> */}
 
                         {/* </Form> */}
+                    </Nav>
+
+                    <Nav className='ms-auto'>
+                        <Nav.Link href="#action1">Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
